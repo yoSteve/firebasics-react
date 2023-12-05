e14e6d7b49cb41646271e2b31009e8b430084f61
Steve Yorke <steve.yorke@redspace.com>
2023-12-05 19:59:37 -0400

    connect app to firestore

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { collection, getFirestore } from 'firebase/firestore';
 import { useAuthState } from 'react-firebase-hooks/auth';
+
 
 import { FIREBASE_CONFIG }  from './secrets';
 import SignIn from './SignIn';
 import './App.css';
+import MessageList from './MessageList';
+import AddMessage from './AddMessage';
 
 const firebaseApp = initializeApp(FIREBASE_CONFIG);
-
 const auth = getAuth(firebaseApp);
+const db = getFirestore(firebaseApp);
+const messagesCollection = collection(db, 'messages')
+
 
 function App() {
   const [user] = useAuthState(auth);
@@ -17,12 +23,18 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Welcome to Firebasics 🔥🔥🔥</h1>
-        <SignIn auth={auth} />
+        <div className="greeting-container">
+          { user  ? (<h2>Hello, {user && user.displayName}.</h2>)
+                  : (<h2>You are not signed in.</h2>) }
+          <SignIn auth={auth} />
+        </div>
       </header>
       <section>
-        { user  ? (<h2>Hello, {user && user.displayName}.</h2>)
-                : (<h2>You are not signed in.</h2>) }
+        <MessageList collection={messagesCollection} />
       </section>
+      <footer>
+        <AddMessage collection={messagesCollection} userName={user.displayName} />
+      </footer>
     </div>
   );
 }
