117ca44db57a9c3c1fda8eef1e50014d8a033fb3
Steve Yorke <steve.yorke@redspace.com>
2023-12-05 20:00:04 -0400

    MessageList  component

---
+++ src/MessageList.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+import { useCollection } from 'react-firebase-hooks/firestore';
+import './MessageList.css';
+
+const MessageList = ({ collection }) => {
+  const [ messages, loading, error ] = useCollection(collection);
+
+
+  const messagesData = messages?.docs.map(doc => {
+    return {
+      id: doc.id,
+      ...doc.data()
+    }
+  });
+
+  return (
+    <div>
+      { error && <strong>Something went wrong</strong> }
+
+      { loading && <strong>Loading...</strong> }
+
+      { messages && (
+        <ul>
+          {messagesData.map(message => {
+            return (
+              <li key={message.id}>
+                <div className='message-text'>{message.text}</div>
+                <div className='message-from'>- {message.from}</div>
+              </li>)
+          })}
+        </ul>
+      )}
+  </div>
+  )
+}
+
+export default MessageList
